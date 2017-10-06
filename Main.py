5cde5c981d9160d5325f4b19290ee45dafbbd82d
iirlas <iirlas@yahoo.com>
2017-10-06 03:10:42 -0500

    Revised Main.py

--- Main.py
+++ Main.py
@@ -15,15 +15,25 @@ class Game(sge.dsp.Game):
 class Room(sge.dsp.Room):
 
     def event_step(self, time_passed, delta_mult):
-        sge.game.project_text(font, "Hello, world!",
-                              sge.game.width / 2,
-                              sge.game.height / 2,
-                              color=sge.gfx.Color("black"), halign="center",
-                              valign="middle")
+
+        # sge.game.project_text(font, "Hello, world!",
+        #                       sge.game.width / 2,
+        #                       sge.game.height / 2,
+        #                       color=sge.gfx.Color("black"), halign="center",
+        #                       valign="middle")
+        pass
+
+
+class Player(sge.dsp.Object):
+    def __init__(self):
+        super().__init__(0, 0, sprite=sprite, checks_collisions=True)
+
+    def event_step(self, time_passed, delta_mult):
+        pass
 
 
 # Create Game object
-Game()
+Game(width=1280, height=720, fps=60, window_text="Leave a mark")
 
 # Create backgrounds
 background = sge.gfx.Background([], sge.gfx.Color("white"))
@@ -31,8 +41,16 @@ background = sge.gfx.Background([], sge.gfx.Color("white"))
 # Load fonts
 font = sge.gfx.Font()
 
+# Load Sprite
+sprite = sge.gfx.Sprite(name="SingleNosferatu", directory="images")
+
+# Create Player
+player = Player()
+
+objects = [player]
+
 # Create rooms
-sge.game.start_room = Room(background=background)
+sge.game.start_room = Room(objects, background=background)
 
 if __name__ == "__main__":
     sge.game.start()
