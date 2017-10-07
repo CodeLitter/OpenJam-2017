f12dc337ff370564aa5a510a73d2c794b78f81c8
iirlas <iirlas@yahoo.com>
2017-10-07 05:41:28 -0500

    Revised Main.py, Revised Player added toggle_crouch, added Obstacle, added Pet, revised Victim revised bounding-box

--- Main.py
+++ Main.py
@@ -11,36 +11,33 @@ import Core
 class Player(sge.dsp.Object):
 
     def __init__(self, x, y, move_speed=1):
-        super().__init__(x, y, sprite=sprite_player,
-                         image_origin_x=sprite_player.width/2,
-                         image_origin_y=sprite_player.height - 20,
-                         checks_collisions=True,
+        super().__init__(x, y, sprite=sprite_player_walk,
+                         image_origin_x=sprite_player_walk.width/2,
+                         image_origin_y=sprite_player_walk.height - 20,
                          collision_precise=True)
         self.move_speed = move_speed
-        self._target = pygame.math.Vector2(self.xstart, self.ystart)
+        self.target = pygame.math.Vector2(self.xstart, self.ystart)
 
     def event_step(self, time_passed, delta_mult):
         position = pygame.math.Vector2(self.x, self.y)
 
-        # Debug
-        if sge.keyboard.get_pressed("space"):
-            randomize_layers(layers)
-        # End Debug
-
-        if sge.mouse.get_pressed("left"):
+        if self.sprite is sprite_player_crouch:
+            pass
+        elif sge.mouse.get_pressed("left"):
             mouse_vec = pygame.math.Vector2(sge.game.mouse.x,
                                             sge.game.mouse.y)
             if position.distance_to(mouse_vec) > self.image_width / 6:
-                self._target.x = mouse_vec.x
-                self._target.y = mouse_vec.y
-        self._target.x = Core.clamp(self._target.x,
-                                    self.image_width / 2,
-                                    ROOM_WIDTH - self.image_width / 2)
-        self._target.y = Core.clamp(self._target.y,
-                                    sge.game.height - FLOOR_HEIGHT,
-                                    sge.game.height)
-        if position.distance_to(self._target) > self.move_speed + 0.1:
-            direction = self.move_speed * (self._target - position).normalize()
+                self.target.x = mouse_vec.x
+                self.target.y = mouse_vec.y
+        self.target.x = Core.clamp(self.target.x,
+                                   self.image_width / 2,
+                                   ROOM_WIDTH - self.image_width / 2)
+        self.target.y = Core.clamp(self.target.y,
+                                   sge.game.height - FLOOR_HEIGHT,
+                                   sge.game.height)
+
+        if position.distance_to(self.target) > self.move_speed + 0.1:
+            direction = self.move_speed * (self.target - position).normalize()
             self.xvelocity = direction.x
             self.yvelocity = direction.y
             self.image_speed = self.move_speed * (time_passed / 1000)
@@ -49,9 +46,53 @@ class Player(sge.dsp.Object):
             self.yvelocity = 0
             self.image_speed = 0
             self.image_index = 0
+        # Flip sprite to face direction
         self.image_xscale = math.copysign(1, self.xvelocity)
-        sge.game.current_room.views[0].x = (self.x - sge.game.width / 2)
+        # adjust view to player's position
+        sge.game.current_room.views[0].x = (self.x - sge.game.width / 4)
         pass
+
+    def toggle_crouch(self, target_x, target_y):
+        if self.sprite is sprite_player_walk:
+            self.sprite = sprite_player_crouch
+            self.target.x = target_x
+            self.target.y = target_y
+        else:
+            self.sprite = sprite_player_walk
+
+
+class Obstacle(sge.dsp.Object):
+
+    def __init__(self, x, y, sprite):
+        super().__init__(x, y, sprite=sprite,
+                         image_origin_x=sprite.width / 2,
+                         image_origin_y=sprite.height,
+                         checks_collisions=True,
+                         collision_ellipse=True)
+        self.bbox_y -= self.image_origin_x
+        self.bbox_x -= self.image_origin_y
+
+    def event_collision(self, other, xdirection, ydirection):
+        if isinstance(other, Player):
+            if Core.Game.key_pressed("space"):
+                # TODO hide player
+                other.toggle_crouch(self.x, self.y)
+
+
+class Pet(sge.dsp.Object):
+
+    def __init__(self, x, y, sprite):
+        super().__init__(x, y, sprite=sprite,
+                         image_origin_x=sprite.width / 2,
+                         image_origin_y=sprite.height,
+                         checks_collisions=True)
+        self.bbox_y -= self.image_origin_x
+        self.bbox_x -= self.image_origin_y
+
+    def event_collision(self, other, xdirection, ydirection):
+        if type(other) is Player:
+            # TODO game over
+            pass
 
 
 class Victim(sge.dsp.Object):
@@ -60,9 +101,9 @@ class Victim(sge.dsp.Object):
         super().__init__(x, y, sprite=sprite,
                          image_origin_x=sprite.width,
                          image_origin_y=sprite.height,
-                         checks_collisions=True,
-                         collision_precise=True)
-    pass
+                         checks_collisions=True)
+        self.bbox_y -= self.image_origin_x
+        self.bbox_x -= self.image_origin_y
 
     def event_collision(self, other, xdirection, ydirection):
         if type(other) is Player:
@@ -76,10 +117,19 @@ ROOM_WIDTH = sge.game.width * 2
 FLOOR_HEIGHT = 200
 
 # Load Sprite
-sprite_player = sge.gfx.Sprite(name="vampWalk",
-                               directory="images")
+sprite_player_walk = sge.gfx.Sprite(name="vampWalk",
+                                    directory="images")
+sprite_player_crouch = sge.gfx.Sprite(name="vampCrouch",
+                                      directory="images")
 sprite_victim = sge.gfx.Sprite(name="victimAsleep",
                                directory="images")
+
+obstacle_sprites = []
+for filename in glob.iglob("./images/tableWquill*.*"):
+    filename = ntpath.basename(filename).split('.')[0]
+    sprite = sge.gfx.Sprite(filename, directory="images")
+    obstacle_sprites.append(sprite)
+
 background_sprites = []
 for filename in glob.iglob("./images/wallPanel*.*"):
     filename = ntpath.basename(filename).split('.')[0]
@@ -88,19 +138,12 @@ for filename in glob.iglob("./images/wallPanel*.*"):
 
 
 # Create backgrounds
-def randomize_layers(layers):
-    for layer in layers:
-        index = random.randrange(len(background_sprites))
-        sprite = background_sprites[index]
-        layer.sprite = sprite
-
-
 layers = []
 for offset in range(int(ROOM_WIDTH / background_sprites[0].width)):
     layer = sge.gfx.BackgroundLayer(None, offset * sprite.width, 0)
     layers.append(layer)
 
-randomize_layers(layers)
+Core.randomize_layers(layers, background_sprites)
 
 background = sge.gfx.Background(layers, sge.gfx.Color("white"))
 
@@ -110,11 +153,17 @@ background = sge.gfx.Background(layers, sge.gfx.Color("white"))
 # Create View
 main_view = sge.dsp.View(0, 0, width=sge.game.width)
 
-# Create Player
-player = Player(-sprite_player.width, main_view.height, move_speed=3)
+# Create Objects
+objects = []
+player = Player(-sprite_player_walk.width, main_view.height, move_speed=3)
+victim = Victim(ROOM_WIDTH, main_view.height, sprite_victim)
 
-objects = [player,
-           Victim(ROOM_WIDTH, main_view.height, sprite_victim)]
+objects.append(player)
+objects.append(victim)
+objects.extend([Obstacle(count * 320,  # obstacle_sprites[0].width,
+                         main_view.height - random.randrange(FLOOR_HEIGHT),
+                         random.choice(obstacle_sprites))
+                for count in range(5)])
 
 # Create rooms
 main_room = Core.Room(objects,
