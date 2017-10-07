b5bf4b653a9991751092266aa6a04266007bf713
iirlas <iirlas@yahoo.com>
2017-10-06 23:31:37 -0500

    Revised Main.py, randomly generates background

--- Main.py
+++ Main.py
@@ -2,6 +2,10 @@
 import sge
 import pygame.math
 import math
+import glob
+import ntpath
+import random
+from pathlib import Path
 import Core
 
 
@@ -25,11 +29,13 @@ class Player(sge.dsp.Object):
             mouse_vec = pygame.math.Vector2(sge.game.mouse.x,
                                             sge.game.mouse.y)
             if position.distance_to(mouse_vec) > self.image_width / 2:
-                self._target.x = sge.game.mouse.x
-                self._target.y = Core.clamp(mouse_vec.y,
-                                            sge.game.height - floor_height,
-                                            sge.game.height) - self.image_height / 2
+                self._target.x = mouse_vec.x
+                self._target.y = mouse_vec.y
 
+        game_offset_height = sge.game.height - self.image_height / 2
+        self._target.y = Core.clamp(self._target.y,
+                                    game_offset_height - floor_height,
+                                    game_offset_height)
         if position.distance_to(self._target) > self.move_speed + 0.1:
             direction = self.move_speed * (self._target - position).normalize()
             self.xvelocity = direction.x
@@ -62,34 +68,44 @@ class Goal(sge.dsp.Object):
 
 # Create Game object
 Core.Game(width=1280, height=720, fps=60, window_text="Leave a mark")
-
-# Create backgrounds
-background_sprites = sge.gfx.Sprite("wallPanel", directory="images")
-layer = sge.gfx.BackgroundLayer(background_sprites, 0, 0, repeat_right=True)
-background = sge.gfx.Background([layer], sge.gfx.Color("white"))
-
-# Load fonts
+ROOM_WIDTH = sge.game.width * 2
 
 # Load Sprite
 sprite_player = sge.gfx.Sprite(name="vampWalk",
                                directory="images")
-
 sprite_goal = sge.gfx.Sprite(name="victimAsleep",
                              directory="images")
+background_sprites = []
+for filename in glob.iglob("./images/wallPanel*.*"):
+    filename = ntpath.basename(filename).split('.')[0]
+    sprite = sge.gfx.Sprite(filename, directory="images")
+    background_sprites.append(sprite)
+
+# Create backgrounds
+layers = []
+for offset in range(int(ROOM_WIDTH / background_sprites[0].width)):
+    index = random.randrange(len(background_sprites))
+    sprite = background_sprites[index]
+    layer = sge.gfx.BackgroundLayer(sprite, offset * sprite.width, 0)
+    layers.append(layer)
+
+background = sge.gfx.Background(layers, sge.gfx.Color("white"))
+
+# Load fonts
+
 
 # Create View
-main_view = sge.dsp.View(0, 0,
-                         width=sge.game.width)
+main_view = sge.dsp.View(0, 0, width=sge.game.width)
 
 # Create Player
-player = Player(5)
+player = Player(3)
 
 objects = [player, Goal()]
 
 # Create rooms
 main_room = Core.Room(objects,
                       views=[main_view],
-                      width=sge.game.width * 2,
+                      width=ROOM_WIDTH,
                       background=background)
 main_room.font = sge.gfx.Font()
 sge.game.start_room = main_room
